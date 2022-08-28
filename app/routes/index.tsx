ebf9e7aa10a635d7c0e6221357cdec9f7081ac09
Brandon Wong <brandonwongck@gmail.com>
2022-08-28 09:48:19 +0800

    adjust hero content

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,37 +3,46 @@ import Typewriter from "typewriter-effect";
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <div className="navbar sticky top-0 z-10 bg-base-100 opacity-90">
+      <div className="navbar sticky top-0 z-10 backdrop-blur-sm">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl normal-case">Home</a>
         </div>
       </div>
 
       <div className="hero min-h-screen bg-base-200 ">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there, I am</h1>
-            <div className="text-5xl font-bold">
+        <div className="hero-content justify-start">
+          <div className="absolute left-10 max-w-md">
+            <div className="text-5xl font-bold">Hello 👋🏻</div>
+            <div className="flex max-w-md whitespace-nowrap text-5xl font-bold">
+              <p className="mr-2">{`I'm`}</p>
               <Typewriter
                 options={{
-                  strings: ["Brandon", "a Geek"],
+                  strings: ["Brandon", "a Software Developer", "a Geek"],
                   autoStart: true,
                   loop: true,
                 }}
               />
             </div>
+
             <header className="group mb-16"></header>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
+
       <div className="mockup-window m-10 border bg-base-300">
-        <div className="flex justify-center bg-base-200 px-4 py-16">Hello!</div>
+        <div className="flex justify-start bg-base-200 px-4 py-16">
+          <div className="flex flex-col">
+            <p>{"> Hi, I'm Blu. Tell me about yourself"}</p>
+            <p>
+              {"> "}
+              <input
+                type="text"
+                className="border-0 bg-transparent focus:outline-none"
+                placeholder=""
+              />
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
