2a8e22250f6d7fffa549e08a90fae5fe46fcbe86
Brandon Wong <brandonwongck@gmail.com>
2022-09-05 00:14:13 +0800

    add theme toggler

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,15 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/outline";
+
 export default function NavBar() {
   return (
-    <div className="navbar sticky top-0 z-10 bg-transparent backdrop-blur-sm">
-      <div className="flex-1">
+    <div className="navbar sticky top-0 z-10 backdrop-blur-sm">
+      <div className="flex-1 justify-between">
         <a className="btn btn-ghost text-xl normal-case">Home</a>
+        <label className="swap-rotate swap mr-4">
+          <input type="checkbox" />
+          <SunIcon data-set-theme="pastel" className="swap-on h-5 w-5" />
+          <MoonIcon data-set-theme="dark" className="swap-off h-5 w-5" />
+        </label>
       </div>
     </div>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 import { Hero, NavBar, Terminal } from "~/components";
 
 export default function Index() {
+  useEffect(() => {
+    themeChange(false);
+    // 👆 false parameter is required for react project
+  }, []);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <NavBar />
