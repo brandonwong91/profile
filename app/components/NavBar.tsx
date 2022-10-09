8ad2b9f14521397f68fc9f3d8108dc717a8334ce
Brandon Wong <brandonwongck@gmail.com>
2022-10-09 17:03:32 +0800

    Change navbar icon

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,7 +4,14 @@ export default function NavBar() {
   return (
     <div className="navbar sticky top-0 z-10 backdrop-blur-sm">
       <div className="flex-1 justify-between">
-        <div className="btn btn-ghost text-xl normal-case" onClick={() => location.reload()}>Home</div>
+        <div className="btn btn-ghost text-xl normal-case" onClick={() => location.reload()}>
+          <div className="w-6 hover:scale-150">
+            <img
+              src={`img/favicon.ico`}
+              alt={""}
+            />
+          </div>
+        </div>
         <label className="swap-rotate swap mr-4">
           <input type="checkbox" />
           <SunIcon data-set-theme="pastel" className="swap-on h-5 w-5" />
