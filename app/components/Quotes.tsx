f40a604d7ba33baa87e161bc291f8c748979c034
Brandon Wong <brandonwongck@gmail.com>
2022-10-09 23:08:30 +0800

    remove timeout implementation

--- app/components/Quotes.tsx
+++ app/components/Quotes.tsx
@@ -7,12 +7,13 @@ export default function Quotes() {
     {
       text: "If I am repeating it, I will automate it.",
       by: "Brandon",
-      icon: <div className="w-4">
-        <img
-          src={`img/favicon.ico`}
-          alt={""}
-        />
-      </div>
+      icon:
+        <div className="w-4">
+          <img
+            src={`img/favicon.ico`}
+            alt={""}
+          />
+        </div>
     },
     {
       text: "It can all be worked out if we talk because everyone [in this world] are friends.",
@@ -27,22 +28,24 @@ export default function Quotes() {
     {
       text: "Keep it simple, stupid.",
       by: "U.S. Navy",
-      icon: <div className="w-4">
-        <img
-          src={`img/icons8-my-computer-100.png`}
-          alt={""}
-        />
-      </div>
+      icon:
+        <div className="w-4">
+          <img
+            src={`img/icons8-my-computer-100.png`}
+            alt={""}
+          />
+        </div>
     },
     {
       text: "If you can't explain it simply, you don't understand it well enough.",
       by: "Albert Einstein",
-      icon: <div className="w-4">
-        <img
-          src={`img/icons8-albert-einstein-48.png`}
-          alt={""}
-        />
-      </div>
+      icon:
+        <div className="w-4">
+          <img
+            src={`img/icons8-albert-einstein-48.png`}
+            alt={""}
+          />
+        </div>
     },
   ]
   const maxQuotes = quotes.length - 1
@@ -64,8 +67,8 @@ export default function Quotes() {
   }
 
   useEffect(() => {
-    window.clearInterval(timer)
-    timer = window.setInterval(navigation, 5 * 1000)
+    // window.clearInterval(timer)
+    // timer = window.setInterval(navigation, 5 * 1000)
     document.addEventListener('touchstart', e => {
       touchStartX = e.changedTouches[0].screenX
     })
@@ -74,15 +77,17 @@ export default function Quotes() {
       touchEndX = e.changedTouches[0].screenX
       checkDirection()
     })
+
+
   }, [selected])
 
   return (
-    <div className="bg-base-200 xs:ml-4 hero flex flex-col min-h-[100vh] justify-center sm:mb-10">
+    <div className="bg-base-200 hero flex flex-col min-h-[100vh] justify-center sm:mb-10">
       <div>
-        <h1 className="sm:ml-12 md:ml-0 sm:mt-10 text-6xl mb-10 self-center text-transparent bg-clip-text bg-gradient-to-tr from-primary to-secondary">
+        <h1 className="hero-content text-6xl text-transparent bg-clip-text bg-gradient-to-tr from-primary to-secondary">
           {`Favorite Quotes`}
         </h1>
-        <div className="flex flex-col absolute">
+        <div className="flex flex-col hero-content">
           <div className="self-center mt-2 flex flex-row cursor-pointer gap-x-1">
             {quotes.map((quote, index) => {
               return (
@@ -93,21 +98,33 @@ export default function Quotes() {
             })}
           </div>
           <div className="flex flex-row w-full">
-            <div className="btn btn-circle btn-sm z-10 self-middle sm:ml-8 md:ml-0"
+            <div className="btn btn-circle btn-sm z-10 self-middle"
               onClick={() => {
                 setSelected(selected === 0 ? maxQuotes : selected - 1);
               }}>
               ❮
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col h-24 indicator">
+              <div className="ml-6 w-8 opacity-50 indicator-item indicator-top indicator-start">
+                <img
+                  src={`img/icons8-quote-left-50.png`}
+                  alt={""}
+                />
+              </div>
               <div className="pt-4 px-4 w-[21rem] italic">
                 {quotes[selected]?.text}
               </div>
-              <div className="text-sm text-end pr-4">
+              <div className="text-sm text-end pr-4 justify-end">
                 {quotes[selected]?.by}
               </div>
+              <div className="mr-6 w-8 opacity-50 indicator-item indicator-bottom indicator-end">
+                <img
+                  src={`img/icons8-get-quote-50.png`}
+                  alt={""}
+                />
+              </div>
             </div>
-            <div className="btn btn-circle btn-sm z-10 align-middle sm:mr-4 md:mr-0"
+            <div className="btn btn-circle btn-sm z-10 align-middle"
               onClick={() => {
                 setSelected(selected === maxQuotes ? 0 : selected + 1);
               }}>
