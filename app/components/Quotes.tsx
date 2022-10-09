ecec14a597ddb2796609b957977353a5b8b8c19c
Brandon Wong <brandonwongck@gmail.com>
2022-10-09 12:29:15 +0800

    Fix responsiveness and added icons

--- app/components/Quotes.tsx
+++ app/components/Quotes.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function Quotes() {
 
@@ -7,34 +7,49 @@ export default function Quotes() {
     {
       text: "If I am repeating it, I will automate it.",
       by: "Brandon",
-      icon: ""
+      icon: <div className="w-4">
+        <img
+          src={`img/favicon.ico`}
+          alt={""}
+        />
+      </div>
     },
     {
       text: "It can all be worked out if we talk because everyone [in this world] are friends.",
       by: "Ms. Kitty White",
-      icon: ""
+      icon: <div className="w-4">
+        <img
+          src={`img/kitty.png`}
+          alt={""}
+        />
+      </div>
     },
     {
       text: "Keep it simple, stupid",
       by: "U.S. Navy",
-      icon: ""
+      icon: <div className="w-4">
+        <img
+          src={`img/icons8-my-computer-100.png`}
+          alt={""}
+        />
+      </div>
     }
   ]
-  const maxQuotes = quotes.length
+  const maxQuotes = quotes.length - 1
   const [selected, setSelected] = useState(0)
-  useEffect(() => {
-    setTimeout(() => {
-      if (selected === maxQuotes - 1) {
-        setSelected(0);
-      } else {
-        setSelected(selected + 1);
-      }
-    }, 5 * 1000);
-  });
+  const navigation = () => {
+    if (selected === maxQuotes) {
+      setSelected(0);
+    } else {
+      setSelected(selected + 1);
+    }
+  }
+  setTimeout(navigation, 5 * 1000)
+
   return (
     <div className="bg-base-200 xs:ml-4 hero flex flex-col min-h-[100vh] justify-center sm:mb-10">
       <div>
-        <h1 className="sm:mt-10 text-6xl mb-10 self-center text-transparent bg-clip-text bg-gradient-to-tr from-primary to-secondary">
+        <h1 className="sm:ml-12 md:ml-0 sm:mt-10 text-6xl mb-10 self-center text-transparent bg-clip-text bg-gradient-to-tr from-primary to-secondary">
           {`Favorite Quotes`}
         </h1>
         <div className="flex flex-col">
@@ -42,31 +57,30 @@ export default function Quotes() {
             {quotes.map((quote, index) => {
               return (
                 <div key={`${index}`} className={clsx("btn btn-xs btn-outline scale-50 hover:scale-100", { "scale-100 bg-gradient-to-tr from-primary to-secondary": selected === index })} onClick={() => setSelected(index)}>
-                  {`${index + 1}`}
+                  {selected === index ? <>{quote.icon}</> : `${index + 1}`}
                 </div>
               )
             })}
           </div>
-          <div className="flex flex-row">
-            <div className="btn btn-circle btn-sm z-10 self-middle"
+          <div className="flex flex-row w-full">
+            <div className="btn btn-circle btn-sm z-10 self-middle sm:ml-8 md:ml-0"
               onClick={() => {
                 setSelected(selected === 0 ? maxQuotes : selected - 1);
               }}>
               ❮
             </div>
             <div className="flex flex-col">
-              <div className="pt-4 px-4 w-80 italic">
-                {quotes[selected].text}
+              <div className="pt-4 px-4 w-[21rem] italic">
+                {quotes[selected]?.text}
               </div>
               <div className="text-sm text-end pr-4">
-                {quotes[selected].by}
+                {quotes[selected]?.by}
               </div>
             </div>
-            <div className="btn btn-circle btn-sm z-10 align-middle"
-              onClick={
-                () => {
-                  setSelected(selected === maxQuotes - 1 ? 0 : selected + 1);
-                }}>
+            <div className="btn btn-circle btn-sm z-10 align-middle sm:mr-4 md:mr-0"
+              onClick={() => {
+                setSelected(selected === maxQuotes ? 0 : selected + 1);
+              }}>
               ❯
             </div>
           </div>
