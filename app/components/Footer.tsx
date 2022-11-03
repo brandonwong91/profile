8a074909d5631ec92bff7fa4a5a44f56034a13c2
Brandon Wong <brandonwongck@gmail.com>
2022-11-04 01:07:06 +0800

    fix hero mobile page and add footer links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,13 +1,33 @@
 export default function Footer() {
+  const techList = [
+    { name: "Remix", href: "https://remix.run/" },
+    { name: "DaisyUI", href: "https://daisyui.com/" },
+    { name: "Vercel", href: "https://vercel.com/" },
+    { name: "NLP.js", href: "https://github.com/axa-group/nlp.js/" },
+  ];
   return (
-    <footer className="footer items-center bg-base-200 p-4 text-neutral-content">
+    <footer className="footer items-center bg-base-200 p-4 text-neutral-content sm:gap-y-2">
       <div className="grid-flow-col items-center">
         <p>Copyright © 2022 Brandon Wong. All rights reserved.</p>
       </div>
-      <code className="text-success">
-        Built with = [ Vercel , DaisyUI , NLP.js , Remix ]
+      <code className="flex text-success">
+        Built = [{" "}
+        <div className="flex gap-x-2">
+          {techList.map((tech, index) => {
+            return (
+              <a
+                className="link-animation text-info"
+                key={index}
+                href={tech.href}
+              >
+                {tech.name}
+              </a>
+            );
+          })}
+        </div>{" "}
+        ]
       </code>
-      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+      <div className="grid-flow-col gap-4 sm:items-center md:place-self-center md:justify-self-end">
         <a
           href={`BrandonWong_CV_2022q3.pdf`}
           download
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -26,7 +26,7 @@ export default function Hero({ setShowGame, showGame }: HeroProps) {
             >
               👋🏻
             </span>
-            <div className="h-[48px] flex-col overflow-y-hidden">
+            <div className="h-[48px] flex-col overflow-y-hidden sm:h-[28px]">
               {helloList.reverse().map((word, index) => {
                 return (
                   <div key={index} className="animate-fade  font-bold">
